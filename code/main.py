636615e24cf151445caeec3cd140838958c9e9ff
BuysDB <git@buysdb.nl>
2019-06-09 23:10:48 +0200

    Added SHT_Array

--- code/main.py
+++ code/main.py
@@ -5,7 +5,9 @@ import Adafruit_ADS1x15
 from mics import MICS
 from mcp230xx import MCP230XX
 import serial
-import
+import SHT31_Array
+
+sht_array = SHT31_Array.SHT31_Array(TCA9548A_address=0x70, N_SHTs =8)
 
 adc = Adafruit_ADS1x15.ADS1115()
 mics = MICS(adc, 0, 1)
@@ -24,13 +26,12 @@ def change_serial_channel(channel=0, serial_mcp):
     serial_mcp.output(2, (channel&0b0100) > 0)
     serial_mcp.output(3, (channel&0b1000) > 0)
 
-
-
-
-
 while True:
     values = mics.aquire_mv()
     print( '%s\t%s' %  tuple(values[x] for x in ['red','ox']), end='' )
     values2 = mics2.aquire_mv()
-    print( '\t%s\t%s' %  tuple(values2[x] for x in ['red','ox']) )
+    print( '\t%s\t%s\t' %  tuple(values2[x] for x in ['red','ox']), end='' )
+
+    print('\t'.join( [str(x) for x in sht_array.read_all_temperature_humidity()]))
+
     time.sleep(0.5)
